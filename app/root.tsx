379df46f7d1929ad01374acc6cafefb30b94416d
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-03 21:07:22 +0300

    Add boilerplate for root

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,31 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { PropsWithChildren } from "react";
 
 export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
 
+export const Document = ({children}: PropsWithChildren) => {
+  return (
+    <html lang="ru">
+      <head>
+        <Meta />
+        <Links />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Отчеты АСКУЭ</title>
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
