4df78c055b0590a26942b82801a1032158fdb94c
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-09 18:02:04 +0300

    Create MainLayout component

---
+++ app/layout/MainLayout.tsx
@@ -0,0 +1,15 @@
+import Header from "~/components/Header";
+import Siderbar from "~/components/Siderbar";
+import Footer from "~/components/Footer";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <Siderbar />
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
