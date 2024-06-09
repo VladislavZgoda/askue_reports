8e7bdce9ec84a3e9eab6469bae2f5719923d8c21
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-09 18:01:27 +0300

    Create Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+
+const Footer = () => {
+  return (
+    <footer
+      className="col-span-5 row-start-5 bg-neutral-200 text-xl
+      border-t-2 border-gray-300 flex items-center justify-center font-bold"
+    >
+      © 2024 Згода Владислав Геннадьевич
+    </footer>
+  )
+};
+
+export default Footer;
