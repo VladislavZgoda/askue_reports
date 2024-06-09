a474c90e82aae0cf2a8d02ccd62853c253e20fb4
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-09 18:02:37 +0300

    Update styles

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,32 +3,37 @@ import logo from '../../public/images/logo.svg';
 const Header = () => {
   return (
     <header
-      className='grid grid-cols-5 items-center
-       bg-neutral-200 p-5'
+      className='
+        grid grid-cols-5 items-center
+      bg-neutral-200 p-5 col-span-5
+        row-span-1 border-b-2 border-gray-300'
     >
       <div
-        className='flex col-span-1 items-center
-          justify-between rounded-lg bg-teal-600 p-3'
+        className='flex col-span-1 items-center justify-self-center w-64
+          justify-around rounded-lg bg-teal-600 p-2'
       >
         <img
-          className='size-12'
+          className='size-10'
           src={logo}
           alt="Изображение счетчика"
         />
 
-        <h1 className="text-3xl font-bold text-white">
+        <h1 className="text-2xl font-bold text-white">
           Отчеты АСКУЭ
         </h1>
       </div>
-      <nav className='text-2xl col-start-3 col-span-3'>
+      <nav className='text-xl col-start-3 col-span-3 mr-6'>
         <menu className='flex justify-between'>
-          <li className='rounded-full bg-sky-600 hover:bg-blue-700 p-4 text-white'>
+          <li className='rounded-2xl bg-sky-600 hover:bg-blue-700
+              p-3 text-white font-semibold '>
             <a href="#">Сформировать отчеты</a>
           </li>
-          <li className='rounded-full bg-sky-600 p-4 hover:bg-blue-700 text-white'>
+          <li className='rounded-2xl bg-sky-600 p-3 font-semibold
+            hover:bg-blue-700 text-white flex items-center'>
             <a href="#">Просмотр данных</a>
           </li>
-          <li className='rounded-full bg-rose-700 hover:bg-rose-800 p-4 text-white'>
+          <li className='rounded-2xl bg-rose-700
+            hover:bg-rose-500 p-3 text-white font-semibold'>
             <a href="#">Выйти</a>
           </li>
         </menu>
