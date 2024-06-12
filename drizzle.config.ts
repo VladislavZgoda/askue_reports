ef416bc90be04f8d4b32b67bc2fd3f533986eca2
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-12 18:22:12 +0300

    Perform initial setup for drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './app/.server/schema.ts',
+  out: './app/.server/migrations',
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+  },
+  verbose: true,
+  strict: true,
+});
