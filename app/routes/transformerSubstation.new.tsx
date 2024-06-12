e21fab045518cc23a90eeac57375d9f36ce8862a
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-12 15:30:04 +0300

    Add styles

--- app/routes/transformerSubstation.new.tsx
+++ app/routes/transformerSubstation.new.tsx
@@ -8,24 +8,42 @@ const CreateNewTransformerSubstation = () => {
   return (
     <main
       className="flex flex-initial items-center justify-center
-      h-full text-2xl"
+      h-full text-3xl"
     >
       <Form
         method="post"
-        className="flex flex-col p-8 h-2/5 w-2/5 flex-initial justify-evenly"
+        className="flex flex-col p-8 h-2/5 w-3/5 flex-initial justify-evenly items-center
+        bg-indigo-100 rounded-lg"
       >
-        <div className="flex flex-initial flex-col items-center">
+        <div
+          className="flex flex-initial flex-col items-center font-semibold
+          focus-within:text-gray-600 text-teal-700"
+        >
           <label htmlFor="name">Наименование</label>
           <input
             type="text"
             name="name"
             id="name"
             placeholder="ТП-1000"
-            className="text-center"
+            className="text-center mt-2 border-4 border-teal-700 rounded-md
+            py-2 px-1 focus:border-gray-500"
           />
         </div>
-        <button type="submit">Создать</button>
-        <button type="button">Отменить</button>
+        <div className="flex flex-initial justify-evenly w-full text-white font-semibold">
+          <button
+            type="submit"
+            className="max-w-fit bg-sky-600 rounded-md py-2 px-5 hover:bg-blue-700"
+          >
+            Создать
+          </button>
+          <button
+            type="button"
+            className="max-w-fit bg-rose-700 rounded-md py-2 px-2 hover:bg-rose-500"
+          >
+            Отменить
+          </button>
+        </div>
+
       </Form>
     </main>
   );
