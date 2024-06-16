ceb99e9d7fb062fcba358045377def7d745c7a3a
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-16 17:07:35 +0300

    Rename and add action

---
+++ app/routes/transformerSubstations.new.tsx
@@ -0,0 +1,62 @@
+import {
+  Form,
+  useActionData,
+  useNavigation
+} from "@remix-run/react";
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { redirect, json } from "@remix-run/node";
+
+import { insertNewTS } from "~/.server/db-queries/transformerSubstationTable";
+
+export const action = async ({
+  request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const transSub = await insertNewTS(formData);
+  return redirect(`/transformerSubstation/${transSub.id}`);
+};
+
+export default function CreateNewTransformerSubstation() {
+  return (
+    <main
+      className="flex flex-initial items-center justify-center
+      h-full text-3xl"
+    >
+      <Form
+        method="post"
+        className="flex flex-col p-8 h-2/5 w-3/5 flex-initial justify-evenly items-center
+        bg-indigo-100 rounded-lg"
+      >
+        <div
+          className="flex flex-initial flex-col items-center font-semibold
+          focus-within:text-gray-600 text-teal-700"
+        >
+          <label htmlFor="name">Наименование</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="ТП-1000"
+            className="text-center mt-2 border-4 border-teal-700 rounded-md
+            py-2 px-1 focus:border-gray-500"
+          />
+        </div>
+        <div className="flex flex-initial justify-evenly w-full text-white font-semibold">
+          <button
+            type="submit"
+            className="max-w-fit bg-sky-600 rounded-md py-2 px-5 hover:bg-blue-700"
+          >
+            Создать
+          </button>
+          <button
+            type="button"
+            className="max-w-fit bg-rose-700 rounded-md py-2 px-2 hover:bg-rose-500"
+          >
+            Отменить
+          </button>
+        </div>
+
+      </Form>
+    </main>
+  );
+}
+
