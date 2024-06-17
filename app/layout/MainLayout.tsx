90a2761a4dd58ac8bf224ec6b03b0837adea13af
zgodavg <153506783+zgodavg@users.noreply.github.com>
2024-06-17 16:49:24 +0300

    Add props

--- app/layout/MainLayout.tsx
+++ app/layout/MainLayout.tsx
@@ -2,11 +2,11 @@ import Header from "~/components/Header";
 import Siderbar from "~/components/Siderbar";
 import Footer from "~/components/Footer";
 
-const MainLayout = () => {
+const MainLayout = (props) => {
   return (
     <>
       <Header />
-      <Siderbar />
+      <Siderbar {...props} />
       <Footer />
     </>
   );
