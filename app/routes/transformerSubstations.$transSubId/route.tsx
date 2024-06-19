378a4c135e45aba1d4dd54d136a0c02ecbec58c0
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-19 21:45:26 +0300

    Add data loader

--- app/routes/transformerSubstations.$transSubId/route.tsx
+++ app/routes/transformerSubstations.$transSubId/route.tsx
@@ -1,9 +1,28 @@
+import { json } from '@remix-run/node';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { selectTransSub } from '~/.server/db-queries/transformerSubstationTable';
+import invariant from 'tiny-invariant';
 
+export const loader = async ({
+  params
+}: LoaderFunctionArgs) => {
+  invariant(params.transSubId, 'Expected params.transSubId');
+  const transSub = await selectTransSub(params.transSubId);
+  if (!transSub) {
+    throw new Response('Not Found', { status: 404 });
+  }
+  return json({ transSub });
+};
 
 export default function TransformerSubstation() {
+  const { transSub } = useLoaderData<typeof loader>();
+
   return (
-    <div>transformerSubstations.$transSubId</div>
-  )
+    <main>
+      { transSub.name }
+    </main>
+  );
 }
 
 
