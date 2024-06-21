085ebf8ef1ad79dd9c597fbefa98eb2eb9e86eaa
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-21 21:23:05 +0300

    Refactor error handling

--- app/routes/transformerSubstations.$transSubId/route.tsx
+++ app/routes/transformerSubstations.$transSubId/route.tsx
@@ -8,10 +8,17 @@ export const loader = async ({
   params
 }: LoaderFunctionArgs) => {
   invariant(params.transSubId, 'Expected params.transSubId');
+
+  if (!Number(params.transSubId)) {
+    throw new Response('Not Found', { status: 404 });
+  }
+
   const transSub = await selectTransSub(params.transSubId);
+
   if (!transSub) {
     throw new Response('Not Found', { status: 404 });
   }
+  
   return json({ transSub });
 };
 
