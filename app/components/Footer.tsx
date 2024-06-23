e8a44645136634a4000c45b1dc7ba53a7f411974
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-23 16:06:37 +0300

    Change styles

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <footer
-      className="col-span-5 row-start-5 bg-indigo-100 text-xl text-slate-700
+      className="col-span-5 row-start-5  text-xl text-slate-700
       border-t-2 border-gray-300 flex items-center justify-center font-bold italic"
     >
       © 2024 Згода Владислав Геннадьевич
