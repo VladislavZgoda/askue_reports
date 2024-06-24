5153c0454edc4a1a17a834d63f5bf077d2d6af81
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-24 21:49:20 +0300

    Change styles

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,37 +4,56 @@ const Header = () => {
   return (
     <header
       className='
-        grid grid-cols-5 items-center
-        bg-indigo-100 p-5 col-span-5
-        row-span-1 border-b-2 border-gray-300'
+        grid grid-cols-5 items-center p-5 col-span-5
+        row-span-1 border-b-2 border-neutral'
     >
       <div
-        className='flex col-span-1 items-center justify-self-end w-64
-          justify-around rounded-lg bg-teal-600 p-2'
+        className='flex col-span-2 items-center justify-self-start w-64
+          justify-around rounded-lg bg-success p-2 ml-14'
       >
         <img
-          className='size-10'
+          className='size-14'
           src={logo}
           alt="Изображение счетчика"
         />
-
         <h1 className="text-2xl font-bold text-white">
           Отчеты АСКУЭ
         </h1>
       </div>
       <nav className='text-xl col-start-3 col-span-3 mr-6'>
-        <menu className='flex justify-between'>
-          <li className='rounded-2xl bg-sky-600 hover:bg-blue-700
-              p-3 text-white font-semibold '>
-            <a href="#">Сформировать отчеты</a>
+        <menu
+          className='menu menu-horizontal bg-base-200 w-full
+          justify-evenly rounded-xl'
+        >
+          <li>
+            <a
+              href="#"
+              role="button"
+              className="btn btn-info btn-outline md:btn-wide
+              btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+            >
+              Сформировать отчеты
+            </a>
           </li>
-          <li className='rounded-2xl bg-sky-600 p-3 font-semibold
-            hover:bg-blue-700 text-white flex items-center'>
-            <a href="#">Просмотр данных</a>
+          <li >
+            <a
+              href="#"
+              role="button"
+              className="btn btn-info btn-outline md:btn-wide
+              btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+            >
+              Просмотр данных
+            </a>
           </li>
-          <li className='rounded-2xl bg-rose-700
-            hover:bg-rose-500 p-3 text-white font-semibold'>
-            <a href="#">Выйти</a>
+          <li>
+            <a
+              href="#"
+              role="button"
+              className="btn btn-error btn-outline md:btn-wide
+              btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+            >
+              Выйти
+            </a>
           </li>
         </menu>
       </nav>
