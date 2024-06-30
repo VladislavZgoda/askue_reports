142ff427ba7088ceb1ed7cb3fb678eefb47410fb
Garry3Fingers <www.vlad78@yandex.ru>
2024-06-30 14:58:39 +0300

    Add form disabled

--- app/routes/transformerSubstations.new/route.tsx
+++ app/routes/transformerSubstations.new/route.tsx
@@ -53,49 +53,54 @@ export default function CreateNewTransformerSubstation() {
       <Form
         method="post"
         action="/transformerSubstations/new"
-        className="flex flex-col p-8 h-2/5 w-3/5 flex-initial justify-evenly items-center
+        className="flex p-8 h-2/5 w-3/5 flex-initial
         bg-neutral-content rounded-lg"
       >
-        <div className="form-control w-full max-w-xs">
-          <label className="label" htmlFor="name">
-            <span className="label-text">Наименование</span>
-          </label>
-          <input
-            type="text"
-            placeholder="ТП-1000"
-            className={
-              `input input-bordered w-full max-w-xs input-xs
+        <fieldset
+          className="flex flex-col justify-evenly items-center w-full h-full flex-initial"
+          disabled={isSubmitting}
+        >
+          <div className="form-control w-full max-w-xs">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Наименование</span>
+            </label>
+            <input
+              type="text"
+              placeholder="ТП-1000"
+              className={
+                `input input-bordered w-full max-w-xs input-xs
                md:input-md sm:input-sm lg:input-lg
                ${actionData?.error ? 'input-error' : 'input-accent'}`
-            }
-            name="name"
-            id="name"
-            defaultValue={actionData?.name}
-          />
-          {actionData?.error ? (
-            <div className="label">
-              <span className="label-text-alt text-error">
-                {actionData.error}
-              </span>
-            </div>
-          ) : null}
-        </div>
-        <div className="flex flex-initial justify-evenly w-full text-white font-semibold">
-          <button
-            type="submit"
-            className="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-          >
-            {isSubmitting ? <span className="loading loading-spinner"></span> : null}
-            {isSubmitting ? 'Создаю...' : 'Создать'}
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-            onClick={() => navigate(-1)}
-          >
-            Назад
-          </button>
-        </div>
+              }
+              name="name"
+              id="name"
+              defaultValue={actionData?.name}
+            />
+            {actionData?.error ? (
+              <div className="label">
+                <span className="label-text-alt text-error">
+                  {actionData.error}
+                </span>
+              </div>
+            ) : null}
+          </div>
+          <div className="flex flex-initial justify-evenly w-full text-white font-semibold">
+            <button
+              type="submit"
+              className="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+            >
+              {isSubmitting ? <span className="loading loading-spinner"></span> : null}
+              {isSubmitting ? 'Создаю...' : 'Создать'}
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+              onClick={() => navigate(-1)}
+            >
+              Назад
+            </button>
+          </div>
+        </fieldset>
       </Form>
     </main>
   );
