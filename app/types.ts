1d334729c256ac96cced1ee58f20ecee5313154d
Garry3Fingers <www.vlad78@yandex.ru>
2024-07-03 21:35:01 +0300

    Add type TransSubName

--- app/types.ts
+++ app/types.ts
@@ -1,6 +1,26 @@
+import type { NavigateFunction } from "@remix-run/react";
+
 export type TransSubs = {
   transSubs: {
     id: number;
     name: string;
   }[]
-}
+};
+
+export type TransSubName = {
+  transSub: {
+    id: number;
+    name: string;
+  } | undefined,
+  isSubmitting: boolean,
+  actionData: {
+    error: string;
+    name: string;
+  } | undefined,
+  navigate: NavigateFunction,
+  formAction: string,
+  buttonNames: {
+    submitName: string;
+    idleName: string;
+  }
+};
