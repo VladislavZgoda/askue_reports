34b67687f22eca11f70b105d5d1676fbd9f04b6a
Garry3Fingers <www.vlad78@yandex.ru>
2024-07-06 16:03:32 +0300

    Add spinner for search

--- app/components/Siderbar.tsx
+++ app/components/Siderbar.tsx
@@ -1,7 +1,8 @@
 import {
   Form,
   NavLink,
-  useSubmit
+  useSubmit,
+  useNavigation
 } from "@remix-run/react";
 import type { TransSubs } from "~/types";
 import { useEffect, useState } from 'react';
@@ -9,6 +10,12 @@ import { useEffect, useState } from 'react';
 const Siderbar = ({ transSubs, q }: TransSubs) => {
   const [query, setQuery] = useState(q || '');
   const submit = useSubmit();
+  const navigation = useNavigation();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      'q'
+    );
 
   useEffect(() => {
     setQuery(q || '');
@@ -75,16 +82,20 @@ const Siderbar = ({ transSubs, q }: TransSubs) => {
               }
               value={query}
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="h-4 w-4 opacity-70">
-              <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd" />
-            </svg>
+            {searching ? (
+              <span className="loading loading-spinner text-info"></span>
+            ): (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70">
+                  <path
+                    fillRule="evenodd"
+                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                    clipRule="evenodd" />
+                </svg>
+            )}
           </label>
         </Form>
         <Form method="post" className="flex-initial">
