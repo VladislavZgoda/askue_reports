4a9c0211dc5a84c957da55dd10409cbe83bb926c
Garry3Fingers <www.vlad78@yandex.ru>
2024-07-17 21:09:23 +0300

    Add queries

---
+++ app/.server/db-queries/notInSystemTable.ts
@@ -0,0 +1,89 @@
+import { db } from "../db";
+import { NotInSystem } from "../schema";
+import type {
+  MetersValues,
+  CheckRecordValues,
+  LastQuantity
+} from "~/types";
+import { eq, and, desc } from "drizzle-orm";
+
+export const insertNotInSystem = async ({
+  quantity,
+  type,
+  date,
+  transformerSubstationId
+}: MetersValues) => {
+  await db
+    .insert(NotInSystem)
+    .values({
+      quantity,
+      type,
+      date,
+      transformerSubstationId
+    });
+};
+
+export const updateNotInSystem = async ({
+  quantity,
+  type,
+  date,
+  transformerSubstationId
+}: MetersValues) => {
+  const updated_at = new Date();
+
+  await db
+    .update(NotInSystem)
+    .set({ quantity, updated_at })
+    .where(
+      and(
+        eq(NotInSystem.transformerSubstationId,
+          transformerSubstationId),
+        eq(NotInSystem.date, date),
+        eq(NotInSystem.type, type)
+      )
+    );
+};
+
+export const selectNotInSystem = async ({
+  type,
+  date,
+  transformerSubstationId
+}: CheckRecordValues): Promise<number | undefined> => {
+  const record = await db
+    .select({
+      quantity: NotInSystem.quantity
+    })
+    .from(NotInSystem)
+    .where(
+      and(
+        eq(NotInSystem.transformerSubstationId,
+          transformerSubstationId),
+        eq(NotInSystem.date, date),
+        eq(NotInSystem.type, type)
+      )
+    );
+
+  return record[0]?.quantity;
+};
+
+export const selectLastQuantity = async ({
+  transformerSubstationId,
+  type
+}: LastQuantity): Promise<number | undefined> => {
+  const record = await db
+    .select({
+      quantity: NotInSystem.quantity
+    })
+    .from(NotInSystem)
+    .where(
+      and(
+        eq(NotInSystem.transformerSubstationId,
+          transformerSubstationId),
+        eq(NotInSystem.type, type)
+      )
+    )
+    .orderBy(desc(NotInSystem.date))
+    .limit(1);
+
+  return record[0]?.quantity;
+};
