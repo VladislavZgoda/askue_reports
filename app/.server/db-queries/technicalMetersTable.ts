b04ea44523bec32bf98af657a58a976e4afa7fc4
Garry3Fingers <www.vlad78@yandex.ru>
2024-07-21 15:56:16 +0300

    Add queries

--- app/.server/db-queries/technicalMetersTable.ts
+++ app/.server/db-queries/technicalMetersTable.ts
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import { db } from "../db";
 import { TechnicalMeters } from "../schema";
 import type { TechnicalMetersValues } from "~/types";
@@ -14,4 +15,35 @@ export const insertTechnicalMeters = async ({
       underVoltage,
       transformerSubstationId
     });
+};
+
+export const selectTechnicalMeters = async (
+  transformerSubstationId: number
+) => {
+  const prevValues = await db
+    .select({
+      quantity: TechnicalMeters.quantity,
+      underVoltage: TechnicalMeters.underVoltage
+    })
+    .from(TechnicalMeters)
+    .where(
+      eq(TechnicalMeters.transformerSubstationId,
+        transformerSubstationId)
+    );
+
+  return prevValues;
+};
+
+export const updateTechnicalMeters = async ({
+  quantity,
+  underVoltage,
+  transformerSubstationId
+}: TechnicalMetersValues) => {
+  await db
+    .update(TechnicalMeters)
+    .set({ quantity, underVoltage })
+    .where(
+      eq(TechnicalMeters.transformerSubstationId,
+        transformerSubstationId)
+    );
 };
