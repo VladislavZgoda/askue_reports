d4881e0d2a9e34ffb98c52da263786b03dd6e055
Garry3Fingers <www.vlad78@yandex.ru>
2024-07-21 15:32:06 +0300

    Implement insertion of technical meters

--- app/.server/db-queries/addTechnicalMeters.ts
+++ app/.server/db-queries/addTechnicalMeters.ts
@@ -1,8 +1,21 @@
 import type { TechnicalMetersAction } from "~/types";
+import { insertTechnicalMeters } from "./technicalMetersTable";
 
 export default async function addTechnicalMeters(
   values: TechnicalMetersAction
 ) {
-  console.log(values);
-
+  const processedValues = handleValues(values);
+  await insertTechnicalMeters(processedValues);
 }
+
+const handleValues = (
+  values: TechnicalMetersAction
+) => {
+  const processedValues = {
+    quantity: Number(values.techMeters),
+    underVoltage: Number(values.underVoltage),
+    transformerSubstationId: Number(values.transSubId)
+  };
+
+  return processedValues;
+};
