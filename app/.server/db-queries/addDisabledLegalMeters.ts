5103a1bc648fabffc9ef9fe25a76a81455e1b5ae
VladislavZgoda <www.vlad78@yandex.ru>
2024-07-22 10:22:21 +0300

    Implement func addDisabledLegalMeters

--- app/.server/db-queries/addDisabledLegalMeters.ts
+++ app/.server/db-queries/addDisabledLegalMeters.ts
@@ -1,7 +1,49 @@
 import type { DisabledLegalMetersAction } from "~/types";
+import { 
+  insertDisabledLegalMeters,
+  selectDisabledLegalMeters,
+  updateDisabledLegalMeters
+ } from "./disabledLegalMetersTable";
+ import { insertMessage } from "./metersActionLogTable";
 
 export default async function addDisabledLegalMeters(
   values: DisabledLegalMetersAction
 ) {
-  console.log(values);
+  const processedValues = handleValues(values);
+  const prevValue = await selectDisabledLegalMeters(
+    processedValues.transformerSubstationId
+  );
+
+  if (prevValue) {
+    const updatedValues = {
+      ...processedValues,
+      quantity: processedValues.quantity + prevValue
+    };
+    await updateDisabledLegalMeters(updatedValues);
+  } else {
+    await insertDisabledLegalMeters(processedValues);
+  }
+
+  await addMessageToLog(values);
 }
+
+const handleValues = (
+  values: DisabledLegalMetersAction
+) => {
+  const processedValues = {
+    quantity: Number(values.disabledMeters),
+    transformerSubstationId: Number(values.transSubId)
+  };
+
+  return processedValues;
+};
+
+const addMessageToLog = async (
+  values: DisabledLegalMetersAction
+) => {
+  const { disabledMeters } = values;
+  const transformerSubstationId = Number(values.transSubId);
+  const time = new Date().toLocaleString('ru');
+  const message = `Отключенные ЮР П2: ${disabledMeters} ${time}`;
+  await insertMessage(message, transformerSubstationId);
+};
