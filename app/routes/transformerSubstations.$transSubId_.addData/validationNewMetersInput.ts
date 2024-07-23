56ce6964d350db7cdfd5583269ed4d89e2f30d41
VladislavZgoda <www.vlad78@yandex.ru>
2024-07-23 10:18:12 +0300

    Create validation func

--- app/routes/transformerSubstations.$transSubId_.addData/validationNewMetersInput.ts
+++ app/routes/transformerSubstations.$transSubId_.addData/validationNewMetersInput.ts
@@ -2,5 +2,26 @@ export default function validateInputNewMeters (
   values: { [k: string]: FormDataEntryValue; }
 ) {
   const errors: { [k: string]: string } = {};
+  if (!values.newMeters) {
+    errors.newMeters = 'Пустое поле, введите число.'
+  }
 
+  if (!values.addedToSystem) {
+    errors.addedToSystem = 'Пустое поле, введите число.'
+  }
+
+  if (!values.type) {
+    errors.type = 'Пустое поле, выберете балансовую принадлежность.'
+  }
+
+  if (!values.date) {
+    errors.date = 'Пустое поле, выберете дату.'
+  }
+
+  if (Number(values.addedToSystem) > Number(values.newMeters)) {
+    errors.difference = `Поле 'Количество новых ПУ' не должно быть меньше,
+      чем поле 'Из них добавлено в систему'.`
+  }
+
+  return errors;
 }
