ec59f2afae172ff574964a64bae0835d4e930ec1
VladislavZgoda <www.vlad78@yandex.ru>
2024-07-29 16:48:14 +0300

    Start doing UI

--- app/routes/transformerSubstations.$transSubId_.changeData/route.tsx
+++ app/routes/transformerSubstations.$transSubId_.changeData/route.tsx
@@ -2,7 +2,7 @@ import type { LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { selectTransSub } from "~/.server/db-queries/transformerSubstationTable";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useFetcher } from "@remix-run/react";
 import LinkToTransSub from "~/components/LinkToTransSub";
 
 export const loader = async ({
@@ -23,8 +23,13 @@ export const loader = async ({
   return json({ transSub });
 };
 
+export const action = async () => {
+
+};
+
 export default function ChangeData() {
   const { transSub } = useLoaderData<typeof loader>();
+  const fetcher = useFetcher<typeof action>();
 
   return (
     <main>
@@ -32,6 +37,151 @@ export default function ChangeData() {
         id={transSub.id}
         name={transSub.name}
       />
+      <div role="tablist" className="tabs tabs-lifted ml-5 mr-5">
+        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="БЫТ" />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          <fetcher.Form className="flex gap-8">
+            <div className="join join-vertical gap-2">
+              <h2 className="join-item text-center">Всего счетчиков</h2>
+              <label className="form-control w-full max-w-xs join-item">
+                <div className="label">
+                  <span className="label-text">Количество ПУ</span>
+                </div>
+                <input 
+                  type="number" 
+                  placeholder="0" 
+                  className="input input-bordered w-full max-w-xs"
+                  aria-label="Количество ПУ" 
+                />
+              </label>
+              <label className="form-control w-full max-w-xs join-item">
+                <div className="label">
+                  <span className="label-text">Из них в системе</span>
+                </div>
+                <input 
+                  type="number" 
+                  placeholder="0" 
+                  className="input input-bordered w-full max-w-xs"
+                  aria-label="Из них в системе" 
+                />
+              </label>
+            </div>
+
+            <div className="join join-vertical gap-2">
+              <h2 className="join-item text-center">Установлено за год</h2>
+              <label className="form-control w-full max-w-xs join-item">
+                <div className="label">
+                  <span className="label-text">Количество ПУ</span>
+                </div>
+                <input 
+                  type="number" 
+                  placeholder="0" 
+                  className="input input-bordered w-full max-w-xs"
+                  aria-label="Количество ПУ" 
+                />
+              </label>
+              <label className="form-control w-full max-w-xs join-item">
+                <div className="label">
+                  <span className="label-text">Из них в системе</span>
+                </div>
+                <input 
+                  type="number" 
+                  placeholder="0" 
+                  className="input input-bordered w-full max-w-xs"
+                  aria-label="Из них в системе" 
+                />
+              </label>
+            </div>
+
+            <div className="join join-vertical gap-2">
+              <h2 className="join-item text-center">Установлено в этом месяце</h2>
+              <label className="form-control w-full max-w-xs join-item">
+                <div className="label">
+                  <span className="label-text">Количество ПУ</span>
+                </div>
+                <input 
+                  type="number" 
+                  placeholder="0" 
+                  className="input input-bordered w-full max-w-xs"
+                  aria-label="Количество ПУ" 
+                />
+              </label>
+              <label className="form-control w-full max-w-xs join-item">
+                <div className="label">
+                  <span className="label-text">Из них в системе</span>
+                </div>
+                <input 
+                  type="number" 
+                  placeholder="0" 
+                  className="input input-bordered w-full max-w-xs"
+                  aria-label="Из них в системе" 
+                />
+              </label>
+            </div>
+
+            <div className="join join-vertical gap-5">
+              <h2 className="join-item text-center">Вышедшие из строя</h2>
+              <label className="form-control w-full max-w-xs join-item">
+                <div className="label">
+                  <span className="label-text">Количество ПУ</span>
+                </div>
+                <input 
+                  type="number" 
+                  placeholder="0" 
+                  className="input input-bordered w-full max-w-xs"
+                  aria-label="Количество ПУ" 
+                />
+              </label>
+              <button className="btn btn-outline btn-success">
+                Изменить данные
+              </button>
+            </div>  
+          </fetcher.Form>
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_2"
+          role="tab"
+          className="tab"
+          aria-label="ЮР"
+          defaultChecked />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          Tab content 2
+        </div>
+
+        <input 
+          type="radio" 
+          name="my_tabs_2" 
+          role="tab" 
+          className="tab" 
+          aria-label="ОДПУ" />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          Tab content 3
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_2"
+          role="tab"
+          className="tab"
+          aria-label="Техучеты"
+          defaultChecked />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          Tab content 4
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_2"
+          role="tab"
+          className="tab"
+          aria-label="Юр отключенные"
+          defaultChecked />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          Tab content 5
+        </div>
+      </div>      
     </main>
   );
 }
