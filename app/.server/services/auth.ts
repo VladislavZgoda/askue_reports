1173fc1e1481ebef3917df15471c3589c56c080b
VladislavZgoda <www.vlad78@yandex.ru>
2024-08-28 13:38:44 +0300

    Add errors handling for login

--- app/.server/services/auth.ts
+++ app/.server/services/auth.ts
@@ -1,9 +1,11 @@
-import { Authenticator } from "remix-auth";
+import { Authenticator, AuthorizationError } from "remix-auth";
 import { sessionStorage } from "./session";
 import { FormStrategy } from "remix-auth-form";
 import { selectUserId } from "../db-queries/users";
 
-export const authenticator = new Authenticator<string>(sessionStorage);
+export const authenticator = new Authenticator<string>(sessionStorage, {
+  throwOnError: true
+});
 
 authenticator.use(
   new FormStrategy(async ({ form }) => {
@@ -12,7 +14,15 @@ authenticator.use(
 
     const user = await selectUserId(userLogin, password);
 
-    return user[0]?.userId;
+    if (!user[0]?.userId) {
+      throw new AuthorizationError("Не верный логин/пароль", {
+        name: "userNotFound",
+        message: "user doesn't exist",
+        cause: "userNotFound",
+      });
+    }
+      
+    return user[0].userId;
   }),
   'user-login'
 );
--- app/routes/login/route.tsx
+++ app/routes/login/route.tsx
@@ -1,6 +1,8 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 import { authenticator } from "~/.server/services/auth";
+import { json } from "@remix-run/node";
+import { AuthorizationError } from "remix-auth";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   return await authenticator.isAuthenticated(request, {
@@ -9,12 +11,23 @@ export async function loader({ request }: LoaderFunctionArgs) {
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-  return await authenticator.authenticate('user-login', request, {
-    successRedirect: "/",
-  });
+  try {
+    return await authenticator.authenticate('user-login', request, {
+      successRedirect: "/",
+    });
+  } catch (error) {
+    if (error instanceof AuthorizationError) {
+      return json({ error: error.message });
+    } else if (error instanceof Response) {
+      // Возвращает response 302
+      return error;
+    }
+  }
 }
 
 export default function Login() {
+  const loginError = useActionData<typeof action>();
+
   return (
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <Form className="card-body" method="post">
@@ -25,10 +38,15 @@ export default function Login() {
           <input
             type="text"
             placeholder="логин"
-            className="input input-bordered"
+            className={`input input-bordered ${loginError?.error && 'input-error'}`}
             id="login"
             name="userLogin"
             required />
+          {loginError?.error && (
+            <div className="label">
+              <span className="label-text-alt text-error">{loginError.error}</span>
+            </div>
+          )}        
         </div>
         <div className="form-control">
           <label htmlFor="password" className="label">
@@ -37,10 +55,15 @@ export default function Login() {
           <input
             type="password"
             placeholder="пароль"
-            className="input input-bordered"
+            className={`input input-bordered ${loginError?.error && 'input-error'}`}
             id="password"
             name="password"
             required />
+          {loginError?.error && (
+            <div className="label">
+              <span className="label-text-alt text-error">{loginError.error}</span>
+            </div>
+          )}   
         </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary" type="submit">
