d3257138a79af20e1670bf0a9638a5ce0620c209
VladislavZgoda <www.vlad78@yandex.ru>
2024-08-28 13:50:43 +0300

    Add h1

--- app/routes/login/route.tsx
+++ app/routes/login/route.tsx
@@ -29,48 +29,53 @@ export default function Login() {
   const loginError = useActionData<typeof action>();
 
   return (
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <Form className="card-body" method="post">
-        <div className="form-control">
-          <label htmlFor="login" className="label">
-            <span className="label-text">Логин</span>
-          </label>
-          <input
-            type="text"
-            placeholder="логин"
-            className={`input input-bordered ${loginError?.error && 'input-error'}`}
-            id="login"
-            name="userLogin"
-            required />
-          {loginError?.error && (
-            <div className="label">
-              <span className="label-text-alt text-error">{loginError.error}</span>
-            </div>
-          )}        
-        </div>
-        <div className="form-control">
-          <label htmlFor="password" className="label">
-            <span className="label-text">Пароль</span>
-          </label>
-          <input
-            type="password"
-            placeholder="пароль"
-            className={`input input-bordered ${loginError?.error && 'input-error'}`}
-            id="password"
-            name="password"
-            required />
-          {loginError?.error && (
-            <div className="label">
-              <span className="label-text-alt text-error">{loginError.error}</span>
-            </div>
-          )}   
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary" type="submit">
-            Войти
-          </button>
-        </div>
-      </Form>
-    </div>
+    <main className="flex flex-col items-center gap-5 w-full">
+      <h1 className="text-4xl font-bold underline text-success">
+        Отчеты АСКУЭ
+      </h1>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <Form className="card-body" method="post">
+          <div className="form-control">
+            <label htmlFor="login" className="label">
+              <span className="label-text">Логин</span>
+            </label>
+            <input
+              type="text"
+              placeholder="логин"
+              className={`input input-bordered ${loginError?.error && 'input-error'}`}
+              id="login"
+              name="userLogin"
+              required />
+            {loginError?.error && (
+              <div className="label">
+                <span className="label-text-alt text-error">{loginError.error}</span>
+              </div>
+            )}        
+          </div>
+          <div className="form-control">
+            <label htmlFor="password" className="label">
+              <span className="label-text">Пароль</span>
+            </label>
+            <input
+              type="password"
+              placeholder="пароль"
+              className={`input input-bordered ${loginError?.error && 'input-error'}`}
+              id="password"
+              name="password"
+              required />
+            {loginError?.error && (
+              <div className="label">
+                <span className="label-text-alt text-error">{loginError.error}</span>
+              </div>
+            )}   
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary" type="submit">
+              Войти
+            </button>
+          </div>
+        </Form>
+      </div>
+    </main>  
   );
 }
