ab9fba6702d22cc71b10f95cd63597c25fd7e752
VladislavZgoda <www.vlad78@yandex.ru>
2024-08-30 11:48:21 +0300

    Refactor component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,11 @@
-const Footer = () => {
+export default function Footer() {
   return (
     <footer
       className="footer footer-center bg-base-300 text-base-content
-      p-4 col-span-5 row-start-5 border-t-2 border-neutral"
-    >
+      p-4 col-span-5 row-start-5 border-t-2 border-neutral">
       <aside>
         <p>Copyright © ${new Date().getFullYear()} - Згода Владислав Геннадьевич</p>
       </aside>
     </footer>
-  )
-};
-
-export default Footer;
+  );
+}
