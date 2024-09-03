044699a5ab250788392211b17b61f4594356e25f
Garry3Fingers <www.vlad78@yandex.ru>
2024-09-03 21:05:59 +0300

    Refactor code

--- app/routes/generate-reports/.server/fillExcel.ts
+++ app/routes/generate-reports/.server/fillExcel.ts
@@ -1,7 +1,6 @@
 import exceljs from 'exceljs';
 import { selectAllTransSubs } from '~/.server/db-queries/transformerSubstationTable';
 import { selectMetersOnDate } from '~/.server/db-queries/electricityMetersTable';
-import fs from 'fs';
 
 type FormDates = {
   [k: string]: FormDataEntryValue;
@@ -9,20 +8,20 @@ type FormDates = {
 
 export default async function fillExcel(dates: FormDates) {
   const excel = new exceljs.Workbook();
-  
-  const templatePath = 'app\\routes\\generate-reports\\.server\\workbooks\\private_sector.xlsx';
-  const copyPath = 'app\\routes\\generate-reports\\.server\\filled-reports\\private_sector.xlsx';
-  
-  fs.copyFile(templatePath, copyPath, (err) => {
-    if (err) throw err;
-  });
+
+  const templatePath = 'app/routes/generate-reports/.server/workbooks/private_sector.xlsx';
+  const copyPath = 'app/routes/generate-reports/.server/filled-reports/private_sector.xlsx';
 
   const privateSectorWB = await excel.xlsx.readFile(templatePath);
   const privateSectorSheet = privateSectorWB.worksheets[0];
 
-  privateSectorSheet.getColumn('A').eachCell(
-    (cell, rowNumber) => {
-      console.log(cell.value, rowNumber);
-    }
-  );
+  privateSectorSheet.getCell('B3').value = 2;
+
+  await excel.xlsx.writeFile(copyPath);
+
+  // privateSectorSheet.getColumn('A').eachCell(
+  //   (cell, rowNumber) => {
+  //     console.log(cell.value, rowNumber);
+  //   }
+  // );
 }
