963f712b3cadd783e9bbd786009bd04e34a1dc4e
VladislavZgoda <www.vlad78@yandex.ru>
2024-09-03 11:44:16 +0300

    Add a fetcher form

--- app/routes/generate-reports/route.tsx
+++ app/routes/generate-reports/route.tsx
@@ -1,5 +1,30 @@
+import { useFetcher } from "@remix-run/react";
+import DateInput from "~/components/DateInput";
+import type { ActionFunctionArgs } from "@remix-run/node";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const dates = Object.fromEntries(formData);
+  
+  return null;
+}
+
 export default function GenerateReports() {
+  const fetcher = useFetcher();
+
   return (
-    <>Test</>
+    <main className="mt-5 ml-10">
+      <p className="mb-3 ml-6">Выберите даты для балансных групп</p>
+      
+      <fetcher.Form className="flex flex-col w-80 gap-2" method="post">
+        <DateInput labelText="Быт" inputName="privateDate" />
+        <DateInput labelText="Юр" inputName="legalDate" />
+        <DateInput labelText="ОДПУ" inputName="odpyDate" />
+        
+        <button className="btn btn-outline btn-primary mt-4" type="submit">
+          Сформировать
+        </button>
+      </fetcher.Form>
+    </main>
   );
 }
