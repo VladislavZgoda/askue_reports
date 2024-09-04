aeb451383b60172354266fc1e303fd1f7164cda9
VladislavZgoda <www.vlad78@yandex.ru>
2024-09-04 16:30:13 +0300

    Refactor code

--- app/routes/generate-reports/.server/fillExcel.ts
+++ app/routes/generate-reports/.server/fillExcel.ts
@@ -16,38 +16,11 @@ export default async function fillExcel(dates: FormDates) {
 
   const transSubs = await selectAllTransSubs();
 
-  const excel = new exceljs.Workbook();
-
-  const templatePath = path + 'workbooks/private_sector.xlsx';
-  const savePath = path + 'filled-reports/private_sector.xlsx';
-
-  const privateSectorWB = await excel.xlsx.readFile(templatePath);
-  const privateSectorSheet = privateSectorWB.worksheets[0];
-  
   const privateMeters = await selectMeters(
     transSubs, 'Быт', dates.privateDate
   );
-  
 
-  privateSectorSheet.getColumn('A').eachCell(
-    (cell, rowNumber) => {
-      const transSub = String(cell.value).trim();
-
-      if (!transSub.startsWith('ТП')) return;
-      
-      if (transSub in privateMeters) {
-        privateSectorSheet
-          .getCell('B' + rowNumber)
-          .value = privateMeters[transSub];
-      } else {
-        privateSectorSheet
-          .getCell('B' + rowNumber)
-          .value = 0;
-      }
-    }
-  );
-
-  await excel.xlsx.writeFile(savePath);
+  await handlePrivateSector(path, privateMeters)
 }
 
 function cleanUp(dirPath: string) {
@@ -73,12 +46,14 @@ type TransSubs = {
   name: string;
 }[];
 
+type Meters = { [k: string]: number };
+
 async function selectMeters(
   transSubs: TransSubs, 
   type: BalanceType, 
   date: FormDataEntryValue,
 ) {
-  const meters: {[k: string]: number} = {};
+  const meters: Meters = {};
 
   for (const transSub of transSubs) {
     const quantity = await selectMetersOnDate({
@@ -91,4 +66,37 @@ async function selectMeters(
   }
 
   return meters;
+}
+
+async function handlePrivateSector(
+  path: string, 
+  privateMeters: Meters
+) {
+  const excel = new exceljs.Workbook();
+
+  const templatePath = path + 'workbooks/private_sector.xlsx';
+  const savePath = path + 'filled-reports/private_sector.xlsx';
+
+  const privateSectorWB = await excel.xlsx.readFile(templatePath);
+  const privateSectorSheet = privateSectorWB.worksheets[0];
+  
+  privateSectorSheet.getColumn('A').eachCell(
+    (cell, rowNumber) => {
+      const transSub = String(cell.value).trim();
+
+      if (!transSub.startsWith('ТП')) return;
+      
+      if (transSub in privateMeters) {
+        privateSectorSheet
+          .getCell('B' + rowNumber)
+          .value = privateMeters[transSub];
+      } else {
+        privateSectorSheet
+          .getCell('B' + rowNumber)
+          .value = 0;
+      }
+    }
+  );
+
+  await excel.xlsx.writeFile(savePath);
 }
