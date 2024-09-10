c95bf6c2b6c00cead8d8f404f95e7cdc3f0bdfc5
VladislavZgoda <www.vlad78@yandex.ru>
2024-09-10 10:25:22 +0300

    Create a func

---
+++ app/routes/generate-reports/.server/createArchive.ts
@@ -0,0 +1,11 @@
+import { zip } from 'zip-a-folder';
+import fs from 'fs';
+
+export default async function createArchive() {
+  const folderPath = 'app/routes/generate-reports/.server/filled-reports';
+  const archivePath = 'app/routes/generate-reports/.server/reports-archive';
+
+  if (!fs.existsSync(archivePath)) fs.mkdirSync(archivePath);
+
+  await zip(folderPath, `${archivePath}/reports.zip`);
+}
