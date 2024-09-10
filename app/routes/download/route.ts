0c37aaed8acd5e1a3116ca3ebe5fb24d20b5bea8
VladislavZgoda <www.vlad78@yandex.ru>
2024-09-10 11:27:38 +0300

    Refactor code

--- app/routes/download/route.ts
+++ app/routes/download/route.ts
@@ -2,13 +2,13 @@ import { createReadableStreamFromReadable } from '@remix-run/node';
 import fs from 'fs';
 
 export const loader = async () => {
-  const filePath = 'app/routes/generate-reports/.server/filled-reports/report.xlsx';
+  const filePath = 'app/routes/generate-reports/.server/reports-archive/reports.zip';
   const file = createReadableStreamFromReadable(fs.createReadStream(filePath));
 
   return new Response(file, {
     headers: {
-      'Content-Disposition': 'attachment; filename="report.xlsx"',
-      'Content-Type': 'text/markdown',
+      'Content-Disposition': `attachment; filename="${encodeURI('Отчеты.zip')}"`,
+      'Content-Type': 'application/zip',
     },
   });
 };
