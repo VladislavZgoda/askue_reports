91d70eb14c666222d389b6425699d541e3c55afa
VladislavZgoda <www.vlad78@yandex.ru>
2024-09-10 12:01:24 +0300

    Create a func for download

--- app/routes/generate-reports/route.tsx
+++ app/routes/generate-reports/route.tsx
@@ -2,18 +2,40 @@ import { useFetcher } from "@remix-run/react";
 import DateInput from "~/components/DateInput";
 import type { ActionFunctionArgs } from "@remix-run/node";
 import fillExcel from "./.server/fillExcel";
+import createArchive from "./.server/createArchive";
+import { useEffect } from "react";
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const dates = Object.fromEntries(formData);
 
   await fillExcel(dates);
+  await createArchive();
   
-  return null;
+  return true;
 }
 
 export default function GenerateReports() {
-  const fetcher = useFetcher();
+  const fetcher = useFetcher<typeof action>();
+  const afterAction = fetcher.data;
+
+  const download = () => {
+    const link = document.createElement('a');
+    link.href = '/download';
+    
+    link.setAttribute(
+      'download',
+      `Отчеты.zip`,
+    );
+    
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode?.removeChild(link);
+  };
+
+  useEffect(() => {
+    if (afterAction) download();
+  }, [afterAction]);
 
   return (
     <main className="mt-5 ml-10">
