8dfbaa96183c064ed1a926f3fb3c94732bc2dd6d
Garry3Fingers <www.vlad78@yandex.ru>
2024-11-13 13:52:57 +0300

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,114 +1,108 @@
-import globals from "globals";
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import react from "eslint-plugin-react";
-import jsxA11Y from "eslint-plugin-jsx-a11y";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import _import from "eslint-plugin-import";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import globals from "globals";
+import importPlugin from "eslint-plugin-import";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import reactPlugin from "eslint-plugin-react";
+import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
-export default [{
-    ignores: ["!**/.server", "!**/.client"],
-}, ...compat.extends("eslint:recommended"), {
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.commonjs,
-        },
+// https://github.com/remix-run/remix/issues/10109
 
-        ecmaVersion: "latest",
-        sourceType: "module",
+const __dirname = new URL(".", import.meta.url).pathname;
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
+export default [
+    js.configs.recommended,
+    importPlugin.flatConfigs.recommended,
+    {
+        ignores: [
+            "build/*",
+            "build/**/*",
+            "**/build/**/*",
+            "eslint.config.mjs",
+            "coverage/*",
+            "coverage/**/*",
+            "node_modules/*",
+            "node_modules/**/*",
+            "global.d.ts",
+        ],
+    },
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+                ...globals.commonjs,
+                ...globals.es6,
+                process: "readonly",
             },
         },
-    },
-}, ...fixupConfigRules(compat.extends(
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-)).map(config => ({
-    ...config,
-    files: ["**/*.{js,jsx,ts,tsx}"],
-})), {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-
-    plugins: {
-        react: fixupPluginRules(react),
-        "jsx-a11y": fixupPluginRules(jsxA11Y),
-    },
-
-    settings: {
-        react: {
-            version: "detect",
+        plugins: {
+            react: reactPlugin,
+            "jsx-a11y": jsxA11yPlugin,
+            "react-hooks": reactHooksPlugin,
         },
-
-        formComponents: ["Form"],
-
-        linkComponents: [{
-            name: "Link",
-            linkAttribute: "to",
-        }, {
-            name: "NavLink",
-            linkAttribute: "to",
-        }],
-
-        "import/resolver": {
-            typescript: {},
+        rules: {
+            ...reactHooksPlugin.configs.recommended.rules,
+            ...reactPlugin.configs.recommended.rules,
+            ...jsxA11yPlugin.configs.recommended.rules,
+            "react/no-unescaped-entities": "off",
+            "react/display-name": "off",
+            "react/prop-types": "off",
+            "no-prototype-builtins": "off",
         },
-    },
-}, ...fixupConfigRules(compat.extends(
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-)).map(config => ({
-    ...config,
-    files: ["**/*.{ts,tsx}"],
-})), {
-    files: ["**/*.{ts,tsx}"],
-
-    plugins: {
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
-        import: fixupPluginRules(_import),
-    },
-
-    languageOptions: {
-        parser: tsParser,
-    },
-
-    settings: {
-        "import/internal-regex": "^~/",
-
-        "import/resolver": {
-            node: {
-                extensions: [".ts", ".tsx"],
+        settings: {
+            react: {
+                version: "detect",
             },
-
-            typescript: {
-                alwaysTryTypes: true,
+            formComponents: ["Form"],
+            linkComponents: [
+                { name: "Link", linkAttribute: "to" },
+                { name: "NavLink", linkAttribute: "to" },
+            ],
+            "import/resolver": {
+                typescript: {
+                    alwaysTryTypes: true,
+                    project: "./tsconfig.json",
+                },
+            },
+            "import/ignore": [".(css)$"],
+        },
+    },
+    // TypeScript configuration
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                tsconfigRootDir: __dirname,
+                project: ["./tsconfig.json"],
+            },
+            globals: {
+                ...globals.node,
+                React: "readonly",
+                NodeJS: "readonly",
             },
         },
-    },
-}, {
-    files: ["**/.eslintrc.cjs"],
-
-    languageOptions: {
-        globals: {
-            ...globals.node,
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
+        rules: {
+            ...typescriptEslint.configs.recommended.rules,
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-require-imports": "off",
+            "@typescript-eslint/no-empty-object-type": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
         },
     },
-}];
+    // Node environment for eslint.config.mjs
+    {
+        files: ["eslint.config.mjs"],
+        env: {
+            node: true,
+        },
+    },
+];
