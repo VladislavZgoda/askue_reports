8caaef5671afeff7593b93c7680206b52b9a4fe9
Garry3Fingers <www.vlad78@yandex.ru>
2024-11-17 14:48:40 +0300

    Create a separate function to generate etag

---
+++ app/utils/etagHash.ts
@@ -0,0 +1,8 @@
+import crypto from 'crypto';
+
+export default function createEtagHash(data: object) {
+  return crypto
+    .createHash('md5')
+    .update(JSON.stringify(data))
+    .digest('hex');
+}
