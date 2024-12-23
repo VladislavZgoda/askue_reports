46a6bc6b43b5d50dbcf4384d6c66b29f181db10a
VladislavZgoda <www.vlad78@yandex.ru>
2024-12-23 13:40:23 +0300

    Reinstall eslint

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: [
+      "build/*", 
+      ".react-router/*",
+      "app/entry.server.tsx",
+    ]
+  },
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
