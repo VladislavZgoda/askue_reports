56a61fda912f5ab1f88d70c8970f1f567bb253f0
Garry3Fingers <www.vlad78@yandex.ru>
2024-12-23 21:05:22 +0300

    Refactor data loading

--- app/routes/view-data/.server/loadData.ts
+++ app/routes/view-data/.server/loadData.ts
@@ -21,65 +21,69 @@ export default async function loadData({
   for (const transSub of transSubs) {
     const transformerSubstationId = transSub.id;
 
-    const privateMeters = await selectMetersOnDate({
-      type: "Быт",
-      date: privateDate,
-      transformerSubstationId,
-    });
-
-    const legalMetersSims = await selectMetersOnDate({
-      type: "ЮР Sims",
-      date: legalDate,
-      transformerSubstationId,
-    });
-
-    const legalMetersP2 = await selectMetersOnDate({
-      type: "ЮР П2",
-      date: legalDate,
-      transformerSubstationId,
-    });
-
-    const odpyMetersSims = await selectMetersOnDate({
-      type: "ОДПУ Sims",
-      date: odpyDate,
-      transformerSubstationId,
-    });
-
-    const odpyMetersP2 = await selectMetersOnDate({
-      type: "ОДПУ П2",
-      date: odpyDate,
-      transformerSubstationId,
-    });
-
-    const notInSystemPrivate = await selectNotInSystemOnDate({
-      type: "Быт",
-      date: privateDate,
-      transformerSubstationId,
-    });
-
-    const notInSystemLegalSims = await selectNotInSystemOnDate({
-      type: "ЮР Sims",
-      date: legalDate,
-      transformerSubstationId,
-    });
-
-    const notInSystemLegalP2 = await selectNotInSystemOnDate({
-      type: "ЮР П2",
-      date: legalDate,
-      transformerSubstationId,
-    });
-
-    const notInSystemOdpySims = await selectNotInSystemOnDate({
-      type: "ОДПУ Sims",
-      date: odpyDate,
-      transformerSubstationId,
-    });
-
-    const notInSystemOdpyP2 = await selectNotInSystemOnDate({
-      type: "ОДПУ П2",
-      date: odpyDate,
-      transformerSubstationId,
-    });
+    const [
+      privateMeters,
+      legalMetersSims,
+      legalMetersP2,
+      odpyMetersSims,
+      odpyMetersP2,
+      notInSystemPrivate,
+      notInSystemLegalSims,
+      notInSystemLegalP2,
+      notInSystemOdpySims,
+      notInSystemOdpyP2,
+    ] = await Promise.all([
+      selectMetersOnDate({
+        type: "Быт",
+        date: privateDate,
+        transformerSubstationId,
+      }),
+      selectMetersOnDate({
+        type: "ЮР Sims",
+        date: legalDate,
+        transformerSubstationId,
+      }),
+      selectMetersOnDate({
+        type: "ЮР П2",
+        date: legalDate,
+        transformerSubstationId,
+      }),
+      selectMetersOnDate({
+        type: "ОДПУ Sims",
+        date: odpyDate,
+        transformerSubstationId,
+      }),
+      selectMetersOnDate({
+        type: "ОДПУ П2",
+        date: odpyDate,
+        transformerSubstationId,
+      }),
+      selectNotInSystemOnDate({
+        type: "Быт",
+        date: privateDate,
+        transformerSubstationId,
+      }),
+      selectNotInSystemOnDate({
+        type: "ЮР Sims",
+        date: legalDate,
+        transformerSubstationId,
+      }),
+      selectNotInSystemOnDate({
+        type: "ЮР П2",
+        date: legalDate,
+        transformerSubstationId,
+      }),
+      selectNotInSystemOnDate({
+        type: "ОДПУ Sims",
+        date: odpyDate,
+        transformerSubstationId,
+      }),
+      selectNotInSystemOnDate({
+        type: "ОДПУ П2",
+        date: odpyDate,
+        transformerSubstationId,
+      }),
+    ]);
 
     data[transSub.name] = {
       id: transSub.id,
