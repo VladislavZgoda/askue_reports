64307814284f310dc8ae23faa65b560ad68bc4bb
VladislavZgoda <www.vlad78@yandex.ru>
2025-03-10 10:34:00 +0300

    Refactor the Select component

--- app/routes/generate-reports/SelectYear.tsx
+++ app/routes/generate-reports/SelectYear.tsx
@@ -4,25 +4,22 @@ export default function SelectYear() {
   const year = cutOutYear(todayDate());
 
   return (
-    <label className="form-control w-full max-w-xs">
-      <div className="label">
-        <span className="label-text">
-          Выберете год для заголовков таблиц Excel
-        </span>
-      </div>
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">
+        Выберете год для заголовков таблиц Excel
+      </legend>
+
       <select
         className="select"
         aria-label="Выберете год для заголовков таблиц Excel"
-        defaultValue=""
+        defaultValue="Выбрать"
         name="year"
         required
       >
-        <option value="" disabled>
-          Выбрать
-        </option>
+        <option disabled={true}>Выбрать</option>
         <option>{year - 1}</option>
         <option>{year}</option>
       </select>
-    </label>
+    </fieldset>
   );
 }
