f16880e58d786492ea044b510986801426a27b76
VladislavZgoda <www.vlad78@yandex.ru>
2025-03-11 10:46:17 +0300

    Refactor the search input

--- app/components/Siderbar.tsx
+++ app/components/Siderbar.tsx
@@ -64,32 +64,36 @@ export default function Siderbar({ transSubs, q }: TransSubs) {
             });
           }}
         >
-          <label className="input input-info flex items-center gap-2">
+          <label className="input input-info">
+            {searching ? (
+              <span className="loading loading-spinner loading-xs text-info"></span>
+            ) : (
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <circle cx="11" cy="11" r="8"></circle>
+                  <path d="m21 21-4.3-4.3"></path>
+                </g>
+              </svg>
+            )}
+
             <input
               type="search"
-              className="grow"
               placeholder="Поиск ТП"
               aria-label="Поиск ТП"
               name="q"
               onChange={(e) => setQuery(e.currentTarget.value)}
               value={query}
             />
-            {searching ? (
-              <span className="loading loading-spinner text-info"></span>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            )}
           </label>
         </Form>
         <Form method="post" className="flex-initial">
