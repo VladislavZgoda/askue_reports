a9679655fa8b1df0907fbb96ebea3fa2c8abef70
VladislavZgoda <www.vlad78@yandex.ru>
2025-05-14 09:25:41 +0300

    Install react-router-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { reactRouterDevTools } from "react-router-devtools";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -10,6 +11,11 @@ export default defineConfig(({ mode }) => {
       "process.env.secret": JSON.stringify(env.secret),
       "process.env.DATABASE_URL": JSON.stringify(env.DATABASE_URL),
     },
-    plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
+    plugins: [
+      reactRouterDevTools(),
+      reactRouter(),
+      tsconfigPaths(),
+      tailwindcss(),
+    ],
   };
 });
