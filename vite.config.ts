1b8e144e191f350f0bab24a8e9bc87b8f4eeca9c
Garry3Fingers <www.vlad78@yandex.ru>
2025-05-18 16:50:13 +0300

    Uninstall react-router-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { reactRouterDevTools } from "react-router-devtools";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -12,7 +11,6 @@ export default defineConfig(({ mode }) => {
       "process.env.DATABASE_URL": JSON.stringify(env.DATABASE_URL),
     },
     plugins: [
-      reactRouterDevTools(),
       reactRouter(),
       tsconfigPaths(),
       tailwindcss(),
