28b05e70afdf1ce216c36091e103451f85e533ad
VladislavZgoda <www.vlad78@yandex.ru>
2025-05-20 16:23:53 +0300

    Create a Button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,13 @@
+import type { ComponentPropsWithoutRef } from "react";
+
+export default function Button({
+  className,
+  ...props
+}: ComponentPropsWithoutRef<"button">) {
+  return (
+    <button
+      {...props}
+      className={`btn btn-xs sm:btn-sm md:btn-md lg:btn-lg ${className}`}
+    />
+  );
+}
