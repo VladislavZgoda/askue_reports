87f6c9aa9ce834d8862ef11e6aba5d9e416230e8
VladislavZgoda <www.vlad78@yandex.ru>
2025-05-21 13:53:05 +0300

    Refactor the Input component

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -2,18 +2,16 @@ import type { ComponentPropsWithoutRef } from "react";
 
 type Props = ComponentPropsWithoutRef<"input"> & {
   error?: string | undefined;
-  legend: string;
 };
 
-export default function Input({ className, error, legend, ...props }: Props) {
+export default function Input({ className, error, ...props }: Props) {
   return (
-    <fieldset className="fieldset">
-      <legend className="fieldset-legend">{legend}</legend>
+    <>
       <input
         {...props}
         className={`input input-xs sm:input-sm md:input-md lg:input-lg ${error && "input-error"} ${className}`}
       />
       {error && <p className="fieldset-label text-error">{error}</p>}
-    </fieldset>
+    </>
   );
 }
