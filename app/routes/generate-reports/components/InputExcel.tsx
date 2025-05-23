8e68e80adecbb929c9fdce47836afe521114a5ba
VladislavZgoda <www.vlad78@yandex.ru>
2025-05-23 13:42:55 +0300

    Refactor the InputExcel component

--- app/routes/generate-reports/components/InputExcel.tsx
+++ app/routes/generate-reports/components/InputExcel.tsx
@@ -4,20 +4,17 @@ type Props = ComponentPropsWithoutRef<"input"> & {
   error?: string | undefined;
 };
 
-export default function InputExcel({ error, ...props }: Props) {
+export default function InputExcel({ error, className, ...props }: Props) {
   return (
-    <fieldset className="fieldset">
-      <legend className="fieldset-legend">
-        Добавить данные из приложения №9
-      </legend>
+    <>
       <input
         {...props}
-        className={`file-input input-xs sm:input-sm md:input-md lg:input-lg ${error ? "file-input-error" : "file-input-info"}`}
+        className={`file-input input-xs sm:input-sm md:input-md lg:input-lg ${error ? "file-input-error" : "file-input-info"} ${className}`}
         aria-label="Добавить данные из приложения №9"
         type="file"
         accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
       />
       {error && <p className="fieldset-label text-error">{error}</p>}
-    </fieldset>
+    </>
   );
 }
