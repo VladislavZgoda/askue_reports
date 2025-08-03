6678817ad1a5ef54faed2958b375ad0d5f35df0d
Garry3Fingers <www.vlad78@yandex.ru>
2025-08-03 15:06:51 +0300

    Add errorClassName prop

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -2,16 +2,24 @@ import type { ComponentPropsWithoutRef } from "react";
 
 type Props = ComponentPropsWithoutRef<"input"> & {
   error?: string | undefined;
+  errorClassName?: string;
 };
 
-export default function Input({ className, error, ...props }: Props) {
+export default function Input({
+  className,
+  error,
+  errorClassName = "",
+  ...props
+}: Props) {
   return (
     <>
       <input
         {...props}
-        className={`input input-xs sm:input-sm md:input-md lg:input-lg ${error && "input-error"} ${className}`}
+        className={`input input-xs sm:input-sm md:input-md lg:input-lg ${error && "input-error"} ${className ?? ""}`}
       />
-      {error && <p className="fieldset-label text-error">{error}</p>}
+      {error && (
+        <p className={`fieldset-label text-error ${errorClassName}`}>{error}</p>
+      )}
     </>
   );
 }
