5c2f5578dc74df530f18678c39fda9a52c60754b
VladislavZgoda <www.vlad78@yandex.ru>
2025-08-08 15:18:13 +0300

    Create component TechnicalMetersTabPanel

---
+++ app/routes/transformer-substations/change-data/components/TechnicalMetersTabPanel.tsx
@@ -0,0 +1,102 @@
+import { useRemixForm } from "remix-hook-form";
+import { useState, useEffect } from "react";
+import { technicalFormResolver as resolver } from "../validation/technical-form.schema";
+
+import Input from "~/components/Input";
+import Fieldset from "~/components/Fieldset";
+import Container from "./Container";
+import TabPanel from "./TabPanel";
+import Button from "~/components/Button";
+
+import type { FetcherWithComponents } from "react-router";
+
+import type {
+  TechnicalFormData,
+  TechnicalFormErrors,
+} from "../validation/technical-form.schema";
+
+interface TechnicalMetersStats {
+  quantity: number;
+  underVoltage: number;
+}
+
+interface PanelProps {
+  errors: TechnicalFormErrors | undefined;
+  action: string;
+  fetcher: FetcherWithComponents<TechnicalFormErrors>;
+  technicalMeters: TechnicalMetersStats;
+  isSubmitting: boolean;
+}
+
+export default function TechnicalMetersTabPanel({
+  errors,
+  action,
+  fetcher,
+  technicalMeters,
+  isSubmitting,
+}: PanelProps) {
+  const { quantity, underVoltage } = technicalMeters;
+
+  const [inputErrors, setInputErrors] = useState(errors);
+
+  useEffect(() => {
+    setInputErrors(errors);
+  }, [errors]);
+
+  const { handleSubmit, register, reset } = useRemixForm<TechnicalFormData>({
+    mode: "onSubmit",
+    resolver,
+    fetcher,
+    defaultValues: {
+      quantity,
+      underVoltage,
+    },
+  });
+
+  return (
+    <TabPanel checked={false} label="Техучёт">
+      <fetcher.Form method="POST" action={action} onSubmit={void handleSubmit}>
+        <div className="flex gap-8">
+          <Container heading="Всего счетчиков">
+            <Fieldset legend="Количество ПУ">
+              <Input
+                type="number"
+                min={0}
+                placeholder="0"
+                error={inputErrors?.quantity?.message}
+                {...register("quantity")}
+              />
+            </Fieldset>
+            <Fieldset legend="Из них под напряжением">
+              <Input
+                type="number"
+                min={0}
+                placeholder="0"
+                error={inputErrors?.underVoltage?.message}
+                {...register("underVoltage")}
+              />
+            </Fieldset>
+          </Container>
+        </div>
+
+        <Button
+          type={isSubmitting ? "button" : "submit"}
+          className={`mt-5 w-50 btn-outline btn-accent ${isSubmitting && "btn-active"}`}
+        >
+          {isSubmitting && <span className="loading loading-spinner"></span>}
+          {isSubmitting ? "Изменение..." : "Изменить данные"}
+        </Button>
+        <Button
+          type="button"
+          className="mt-5 ml-5 w-50 btn-neutral btn-outline"
+          onClick={() => {
+            reset();
+            setInputErrors(undefined);
+          }}
+        >
+          Очистить форму
+        </Button>
+      </fetcher.Form>
+    </TabPanel>
+  );
+}
