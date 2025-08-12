95bc5ae3bb6fa21b31504e76a86eea592a47b653
VladislavZgoda <www.vlad78@yandex.ru>
2025-08-12 10:30:10 +0300

    Rename file

--- app/routes/transformer-substations/change-data/change-billing-meters.action.ts
+++ app/routes/transformer-substations/change-data/change-billing-meters.action.ts
@@ -4,7 +4,7 @@ import type { BillingFormData } from "./validation/billing-form.schema";
 import { getValidatedFormData } from "remix-hook-form";
 import { billingFormResolver } from "./validation/billing-form.schema";
 
-import changeData from "./.server/db-actions/update-billing-meters";
+import changeData from "./.server/db-actions/upsert-billing-meters";
 
 export async function action({ request, params }: Route.ActionArgs) {
   const {
