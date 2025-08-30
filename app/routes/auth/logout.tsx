9c6531a6d6057623416359c55d5d258d5e3a7c03
Garry3Fingers <www.vlad78@yandex.ru>
2025-08-30 12:41:24 +0300

    Refactor code

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/logout";
 import sessionStorage from "~/.server/services/session";
-import { redirect } from "react-router";
+import { href, redirect } from "react-router";
 import { isNotAuthenticated } from "~/.server/services/auth";
 
 export async function loader({ request }: Route.LoaderArgs) {
@@ -11,7 +11,8 @@ export async function action({ request }: Route.ActionArgs) {
   const session = await sessionStorage.getSession(
     request.headers.get("cookie"),
   );
-  return redirect("/login", {
+
+  return redirect(href("/login"), {
     headers: { "Set-Cookie": await sessionStorage.destroySession(session) },
   });
 }
