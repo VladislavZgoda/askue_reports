cf174686d363d4bf1926aab99430533ae9d244c9
VladislavZgoda <www.vlad78@yandex.ru>
2026-06-09 15:46:44 +0300

    chore: uninstall vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig, loadEnv } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
@@ -10,6 +9,9 @@ export default defineConfig(({ mode }) => {
       "process.env.secret": JSON.stringify(env.secret),
       "process.env.DATABASE_URL": JSON.stringify(env.DATABASE_URL),
     },
-    plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
+    plugins: [reactRouter(), tailwindcss()],
+    resolve: {
+      tsconfigPaths: true,
+    },
   };
 });
